619ff8a6cc38d0e966d3fbbe7770c50f9d6662ab
Tim Kelty <tim@fusionary.com>
2019-10-14 15:54:51 -0400

    Initial conversion

--- src/Plugin.php
+++ src/Plugin.php
@@ -13,7 +13,10 @@ class Plugin extends \craft\base\Plugin
     protected function settingsHtml(): string
     {
         return Craft::$app->getView()->renderTemplate("{$this->handle}/settings", [
-            'settings' => $this->getSettings()
+            'settings' => $this->getSettings(),
+            'ssoUrl' => \craft\helpers\UrlHelper::siteUrl(
+                implode('/', [Craft::$app->getConfig()->getGeneral()->actionTrigger, $this->handle, 'login'])
+            ),
         ]);
     }
 }
---
+++ src/controllers/LoginController.php
@@ -0,0 +1,38 @@
+<?php
+namespace timkelty\craftcms\discoursesso\controllers;
+
+use Craft;
+use craft\helpers\UrlHelper;
+use Cviebrock\DiscoursePHP\SSOHelper;
+use timkelty\craftcms\discoursesso\Plugin;
+use yii\web\BadRequestHttpException;
+
+class LoginController extends \craft\web\Controller
+{
+    public function actionIndex()
+    {
+        $this->requireLogin();
+
+        $settings = Plugin::getInstance()->getSettings();
+        $user = Craft::$app->getUser()->getIdentity();
+        $payload = Craft::$app->getRequest()->getRequiredParam('sso');
+        $signature = Craft::$app->getRequest()->getRequiredParam('sig');
+        $sso = (new SSOHelper())->setSecret($settings->ssoSecret);
+
+        if ( !($sso->validatePayload($payload, $signature)) ) {
+            throw new BadRequestHttpException('Invalid SSO Payload.');
+        }
+
+        $query = $sso->getSignInString(
+            $sso->getNonce($payload),
+            $user->id,
+            $user->email,
+            [
+                'username' => $user->username,
+                'name'     => $user->getName(),
+            ]
+        );
+
+        $this->redirect(UrlHelper::urlWithParams($settings->ssoEndpoint, $query));
+    }
+}
